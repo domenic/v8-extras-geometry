6c836fed621b4bf987d7a7754ae70022b4b93e0e
Domenic Denicola <d@domenic.me>
2017-01-04 18:35:22 -0500

    Fix unqualified global usage

--- web-idl-setup.js
+++ web-idl-setup.js
@@ -4,8 +4,8 @@
   binding.webIDL = {
     brandsSymbol: v8.createPrivateSymbol('WebIDL interface brands'),
     implSymbol: v8.createPrivateSymbol('WebIDL implementation'),
-    implsToWrappers: Object.create(null),
-    dictionaries: Object.create(null),
+    implsToWrappers: global.Object.create(null),
+    dictionaries: global.Object.create(null),
     conversions: {
       'unrestricted double'(x) {
         return +x;
